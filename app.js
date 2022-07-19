584d9ddccfc66884af0efb33f54155986c08450d
Ashwani Kumar <ashwanikr160@gmail.com>
2022-07-20 05:27:06 +0530

    profile update done

--- app.js
+++ app.js
@@ -19,12 +19,8 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(
-  cors({
-    origin: ['http://localhost:3300'],
-    credentials: true,
-  })
-);
+// enabling anyone to access api resources
+app.use(cors({}));
 
 app.use(authRoutes);
 
