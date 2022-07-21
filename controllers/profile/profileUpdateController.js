23afbbad405dc7262f26ff3acf4a167675b6cda2
Ashwani Kumar <ashwanikr160@gmail.com>
2022-07-21 23:44:51 +0530

    comment and replied comment added

--- controllers/profile/profileUpdateController.js
+++ controllers/profile/profileUpdateController.js
@@ -1,3 +1,4 @@
+const User = require('../../models/User');
 const handleErrors = require('../utils/handleErrors');
 
 module.exports = async (req, res) => {
@@ -8,26 +9,25 @@ module.exports = async (req, res) => {
         .status(400)
         .json('name or file or username should be provided to update profile.');
 
-    if (name) req.userInfo.name = name;
-    if (req.imageURL) req.userInfo.photoUrl = req.imageURL;
+    const user = await User.findOne({ _id: req.userInfo._id }).exec();
+
+    if (name) user.name = name;
+    if (req.imageURL) user.photoUrl = req.imageURL;
     if (username) {
-      if (
-        Date.now() - req.userInfo.usernameChangedTimestamp <
-        60 * 24 * 60 * 60 * 1000
-      )
+      if (Date.now() - user.usernameChangedTimestamp < 60 * 24 * 60 * 60 * 1000)
         return res
           .status(403)
           .json(
             `You can change username after ${Math.ceil(
-              (req.userInfo.usernameChangedTimestamp +
+              (user.usernameChangedTimestamp +
                 60 * 24 * 60 * 60 * 1000 -
                 Date.now()) /
                 (24 * 60 * 60 * 1000)
             )} days`
           );
-      req.userInfo.username = username;
+      user.username = username;
     }
-    await req.userInfo.save();
+    await user.save();
     res.status(200).json('profile updated successfully.');
   } catch (err) {
     const message = handleErrors(err);
